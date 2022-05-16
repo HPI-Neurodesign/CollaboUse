8fa4deb1a58a9b5f9858ad85b95662f3e14b1dbf
Corinna Jaschek <corinna.jaschek@gmail.com>
2022-05-16 14:40:21 +0200

    allow custom room ids

--- src/IntroPage.tsx
+++ src/IntroPage.tsx
@@ -68,7 +68,19 @@ export function IntroPage({
         ))}
         <Button
           className="room-button"
-          onClick={() => joinRoom()}
+          onClick={() => {
+            var roomName = prompt("Please enter your identifier") ?? "";
+            roomName = roomName
+              .replace(/[&/\\#,+()$~%.'":*?<>{}]/g, "_")
+              .replace(/\u00df/g, "ss")
+              .replace(/\u00e4/g, "ae")
+              .replace(/\u00f6/g, "oe")
+              .replace(/\u00fc/g, "ue")
+              .replace(/\u00c4/g, "Ae")
+              .replace(/\u00d6/g, "Oe")
+              .replace(/\u00dc/g, "Ue");
+            joinRoom(roomName);
+          }}
           variant="contained"
         >
           Join New Room
